1e2c26bd2443d1dce9adcfd9c0a343ae4966a472
Cheryet <C.heryet@outlook.com>
2022-11-30 14:41:12 -0700

    update InterviewerList story

--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,7 @@ import DayListItem from "components/DayListItem";
 import DayList from 'components/DayList'
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
@@ -142,3 +143,12 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+//Appointment Stories 
+
+storiesOf('Appointments', module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
