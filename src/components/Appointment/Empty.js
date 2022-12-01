2b951f3d492844c36ede542f2562afaaa65c4af9
Cheryet <C.heryet@outlook.com>
2022-11-30 18:08:30 -0700

    update props

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,15 +1,18 @@
 import React from "react";
 
-const Empty = (props) => {
-  <main className="appointment__add" >
-  <img
-    className="appointment__add-button"
-    src="images/add.png"
-    alt="Add"
-    onClick={props.onAdd}
-  />
-</main>
 
+const Empty = (props) => {
+  return (
+    <main className="appointment__add" >
+    <img
+      className="appointment__add-button"
+      src="images/add.png"
+      alt="Add"
+      onClick={props.onAdd}
+    />
+  </main>
+
+  );
 }
 
 export default Empty;
