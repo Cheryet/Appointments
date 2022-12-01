ec8daac6ce14c0a5de03d8962a4d35460f98475d
Cheryet <C.heryet@outlook.com>
2022-11-30 18:09:02 -0700

    update props and conditionals

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,10 +5,14 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty'
 
+
 const Appointment = (props) => {
   
   return(
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}></Header>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
   );
 }
 
