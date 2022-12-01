7339368c69b158ab73a37b060a4fb3517c33c610
Cheryet <C.heryet@outlook.com>
2022-11-30 18:09:22 -0700

    update stories for form

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,10 @@ import Appointment from "components/Appointment/index.js";
 import Header from 'components/Appointment/Header'
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+import { Fragment } from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
@@ -159,6 +163,25 @@ storiesOf('Appointments', module)
   .add("Header", () => <Header time={'12pm'} />)
   .add('Empty', () => <Empty onAdd={action("onAdd")}/>)
   .add('Show', () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
-  .add('Confirm', () => <Confirm />)
-
+  .add('Confirm', () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")} onCancel={action('onCancel')}/>)
+  .add("Status", () => <Status message={"Deleting"} />)
+  .add("Error", () => <Error message={"Could not delete appointment."} onClose={action('onClose')} />)
+  .add("Form on Edit", () => <Form student={'Corbin'} interviewer={1} interviewers={interviewers} onSave={action('onSave')} onCancel={action('onCancel')} />)
+  .add("Form on Create", () => <Form interviewers={interviewers} onSave={action('onSave')} onCancel={action('onCancel')}  />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
 
