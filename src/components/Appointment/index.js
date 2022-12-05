31b860c3447aab1a5538c5cb7f014851f352cb24
Cheryet <C.heryet@outlook.com>
2022-12-05 15:19:16 -0700

    implement save funtion for appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,40 +1,53 @@
-import 'components/Appointment/styles.scss'
-import React from 'react';
-import Header from './Header';
-import Show from './Show';
-import Empty from './Empty'
-import useVisualMode from 'hooks/useVisualMode';
-import Form from './Form';
+import "components/Appointment/styles.scss";
+import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
-
-
+// ~ Appointment Component ~
 const Appointment = (props) => {
-
   //Mode Constants
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
-  const CREATE = 'CREATE';
+  const CREATE = "CREATE";
 
   const { mode, transition, back } = useVisualMode(
-    props.interview ? SHOW : EMPTY 
+    props.interview ? SHOW : EMPTY
   );
-  
-  return(
+
+  //Saves/Books new appointment
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    props.bookInterview(props.id, interview);
+    transition(SHOW);
+  }
+
+  return (
     <article className="appointment">
       <Header time={props.time}></Header>
-        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-        {mode === SHOW && (
-          <Show
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          />
-          )}
-          {mode === CREATE && <Form interviewers={props.interviewers} onCancel={()=>{back()}}  />}
-
-      {/* {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />} */}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={props.interviewers}
+          onCancel={() => {
+            back();
+          }}
+          onSave={(student, interviewer) => save(student, interviewer)}
+        />
+      )}
     </article>
   );
-}
-
+};
 
 export default Appointment;
