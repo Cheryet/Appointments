373f6cb7541644559002516afe977a7480d36a62
Cheryet <C.heryet@outlook.com>
2022-12-05 17:49:17 -0700

    implemented Edit and delete buttons

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -16,18 +16,18 @@ const Show = (props) => {
             className="appointment__actions-button"
             src="images/edit.png"
             alt="Edit"
-            onClick={props.onEdit}
+            onClick={() => props.onEdit()}
           />
           <img
             className="appointment__actions-button"
             src="images/trash.png"
             alt="Delete"
-            onClick={props.onDelete}
+            onClick={() => props.onDelete()}
           />
         </section>
       </section>
     </main>
   );
-}
+};
 
 export default Show;
