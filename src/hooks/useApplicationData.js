72c565949b553881ae6de9cf03154c834c4501f6
Cheryet <C.heryet@outlook.com>
2022-12-06 17:33:22 -0700

    reorganize hook directory into src

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,10 +10,7 @@ const useApplicationData = () => {
     interviewers: {},
   });
 
-  //Sets Day State
-  const setDay = (day) => setState({ ...state, day });
-
-  //Ajax GET requests
+  //Ajax GET requests to populate state
   useEffect(() => {
     //Get request Promises
     const daysPromise = Axios.get("/api/days");
@@ -36,6 +33,9 @@ const useApplicationData = () => {
     );
   }, []);
 
+  //Sets Day State
+  const setDay = (day) => setState({ ...state, day });
+
   //Ajax PUT request ~ Updates the API and state object with the new appointment
   function bookInterview(id, interview) {
     const appointment = {
@@ -47,10 +47,16 @@ const useApplicationData = () => {
       [id]: appointment,
     };
 
+    const [today, spots] = getSpots(state, appointments);
+    const day = { ...state.days[today], spots: spots };
+    const days = [...state.days];
+    days.splice(today, 1, day);
+
     return Axios.put(`/api/appointments/${id}`, appointment).then(
       setState({
         ...state,
         appointments,
+        days,
       })
     );
   }
@@ -69,13 +75,37 @@ const useApplicationData = () => {
       [id]: appointment,
     };
 
+    const [today, spots] = getSpots(state, appointments);
+    const day = { ...state.days[today], spots: spots };
+    const days = [...state.days];
+    days.splice(today, 1, day);
+
     return Axios.delete(`/api/appointments/${id}`)
       .then(
         setState({
           ...state,
           appointments,
+          days,
         })
       )
       .catch(setState({ ...state }));
   };
+
+  //Gets total spots for day
+  const getSpots = (state, newAppointements) => {
+    const dayIndex = state.days.findIndex((day) => day.name === state.day);
+    const currentDay = state.days[dayIndex];
+    const listOfAppointmentIds = currentDay.appointments;
+
+    const listOfFreeAppointments = listOfAppointmentIds.filter(
+      (id) => !newAppointements[id].interview
+    );
+
+    const spots = listOfFreeAppointments.length;
+    return [dayIndex, spots];
+  };
+
+  return { state, setDay, bookInterview, cancelInterview };
 };
+
+export default useApplicationData;
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const useVisualMode = (initial) => {
+
+  const [mode, setMode] = useState(initial)
+
+  //Sets history state to an array with the param value
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+
+    if (replace){
+      return setMode(mode);
+    }
+    setMode(mode)
+    //adds param to end off history array
+    setHistory((prev) => [...prev, mode])
+  }
+
+  //Removes last element of history array and sets mode to new last element, if only one element in array set mode to remaining element
+  const back = () => {
+
+    if (history.length === 1) {
+      return setMode(history[0]);
+    }
+
+    history.pop()
+    let index = history.length - 1
+
+    setMode(history[index])
+
+  }
+
+  return { mode, transition, back}
+
+}
+
+
+
+export default useVisualMode;
