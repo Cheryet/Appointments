2f6eadaa39b6ec91b9f9677893b8c1df80f06a72
Cheryet <C.heryet@outlook.com>
2022-12-07 20:57:42 -0700

    add comments

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,25 +1,23 @@
 import React from "react";
 import classNames from "classnames";
-import "components/InterviewerListItem.scss"
-
+import "components/InterviewerListItem.scss";
 
 const InterviewerListItem = (props) => {
-
+  //updates interviwer icon based on action
   const interviewerClass = classNames("interviewers__item", {
-    "interviewers__item--selected ": props.selected
+    "interviewers__item--selected ": props.selected,
   });
-
 
   return (
     <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
-      className="interviewers__item-image"
-      src={props.avatar}
-      alt={props.name}
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
       />
       {props.selected && <>{props.name}</>}
     </li>
   );
-}
+};
 
 export default InterviewerListItem;
