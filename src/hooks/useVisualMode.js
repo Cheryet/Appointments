a29f5da107dec50c9f0396818195bba72d257d75
Cheryet <C.heryet@outlook.com>
2022-12-11 10:44:43 -0700

    update back function to copy state first, then modify

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,40 +1,35 @@
 import { useState } from "react";
 
 const useVisualMode = (initial) => {
-
-  const [mode, setMode] = useState(initial)
+  const [mode, setMode] = useState(initial);
 
   //Sets history state to an array with the param value
   const [history, setHistory] = useState([initial]);
 
   const transition = (mode, replace = false) => {
-
-    if (replace){
+    if (replace) {
       return setMode(mode);
     }
-    setMode(mode)
+    setMode(mode);
     //adds param to end off history array
-    setHistory((prev) => [...prev, mode])
-  }
+    setHistory((prev) => [...prev, mode]);
+  };
 
   //Removes last element of history array and sets mode to new last element, if only one element in array set mode to remaining element
   const back = () => {
-
     if (history.length === 1) {
       return setMode(history[0]);
     }
 
-    history.pop()
-    let index = history.length - 1
+    let newHistory = [...history];
 
-    setMode(history[index])
+    newHistory.pop();
+    let index = newHistory.length - 1;
 
-  }
+    setMode(newHistory[index]);
+  };
 
-  return { mode, transition, back}
-
-}
-
-
+  return { mode, transition, back };
+};
 
 export default useVisualMode;
