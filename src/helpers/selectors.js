3bca963842d7dfded1aa6b0af4987b54f79185a7
Cheryet <C.heryet@outlook.com>
2022-12-11 12:49:58 -0700

    update getAppointmentsForDay func to follow best practices

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,62 +1,45 @@
-
 //returns an array of appointments for the day
 export function getAppointmentsForDay(state, day) {
+  let appointArr = [];
 
-  let appointArr = []
-
-  state.days.map((element) => {
+  state.days.forEach((element) => {
     if (element.name === day) {
       element.appointments.forEach((appoint) => {
-        appointArr.push(state.appointments[appoint])
-
-      })
+        appointArr.push(state.appointments[appoint]);
+      });
     }
-  })
+  });
 
   return appointArr;
-
 }
 
 // returns an array of Interviewers for day
 
 export function getInterviewersForDay(state, day) {
-
-  let interviewersArr = []
+  let interviewersArr = [];
 
   //Find matching date
   state.days.forEach((obj) => {
     if (obj.name === day) {
-
       //Pull interviewer ID's from array
       obj.interviewers.forEach((interviewerID) => {
-
         //Push interviewers Object from that day to a new array
-        interviewersArr.push(state.interviews[interviewerID])
-      })
+        interviewersArr.push(state.interviews[interviewerID]);
+      });
     }
-  })
+  });
 
   return interviewersArr;
-
 }
 
-
-
-
 export function getInterview(state, interview) {
-
-  if (!interview){
+  if (!interview) {
     return null;
   }
 
   let obj = {};
-  obj.student = interview.student
-  obj.interviewer = state.interviews[interview.interviewer]
+  obj.student = interview.student;
+  obj.interviewer = state.interviews[interview.interviewer];
 
   return obj;
-
-
-
 }
-
-
